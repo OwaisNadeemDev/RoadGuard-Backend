5b233effbbe1dcbf75db84deee7e6f259fb4f308
Owais Nadeem <owaisnadeem.developer@gmail.com>
2024-08-15 01:18:24 +0500

    -implemented cors

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/owaisnadeemdev/roadguard
 go 1.22
 
 require (
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/gorilla/mux v1.8.1
+	github.com/lib/pq v1.10.9
+	github.com/rs/cors v1.11.0
 	golang.org/x/crypto v0.26.0
 )
--- internal/api/http/server/router.go
+++ internal/api/http/server/router.go
@@ -1,12 +1,22 @@
 package server
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/owaisnadeemdev/roadguard/internal/api/http/handlers"
+	"github.com/rs/cors"
 )
 
-func NewRouter() *mux.Router {
+func NewRouter() http.Handler {
 	router := mux.NewRouter()
 	router.HandleFunc("/signup", handlers.SignupHandle)
-	return router
+
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:8100"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Content-Type", "Authorization"},
+	}).Handler(router)
+
+	return corsHandler
 }
